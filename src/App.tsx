f592fc30d301a80e9c917f13e2870bbd9437352c
Haemoffy <fuwansuu@gmail.com>
2024-09-30 22:40:01 +0800

    changes: - made a function to detect guess keys and add then reveal it rto HangmanWord

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import HangmanModel from "./HangmanModel";
 import HangmanWord from "./HangmanWord";
 import words from './wordlist.json'
@@ -8,11 +8,33 @@ function App() {
   const [wordToGuess, setwordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const addGuessedLetter = useCallback((letter: string) => {
+    if (guessedLetters.includes(letter)) return
+
+    setGuessedLetters(prevLetter => [...prevLetter, letter])
+  }, [guessedLetters])
+
+  useEffect(() => {
+    const handleKeyTrigger = (e: KeyboardEvent) => {
+      const key = e.key
+      if (!key.match(/^[a-z]$/)) return
+
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener('keypress', handleKeyTrigger)
+
+    return () => {
+      document.removeEventListener('keypress', handleKeyTrigger)
+    }
+  }, [guessedLetters])
 
   return (
     <div className="container">
       <HangmanModel />
-      <HangmanWord wordToGuess={wordToGuess} />
+      <HangmanWord wordToGuess={wordToGuess} guessedLetters={guessedLetters} />
     </div>
   )
 }
