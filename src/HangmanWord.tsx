b1ff8959082e0e65940b1ee4cd057fcdca908840
Haemoffy <fuwansuu@gmail.com>
2024-10-01 22:31:32 +0800

    changes: - toggle active for revealing correct guessed letter - toggle reveal for revealing unguessed letter

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -3,17 +3,24 @@ import "./assets/css/HangmanWord.css"
 type HangmanWordProp = {
     wordToGuess: string,
     guessedLetters: string[]
+    incorrectLettersLength: number
 }
 
-function HangmanWord({ wordToGuess, guessedLetters }: HangmanWordProp) {
+function HangmanWord({ wordToGuess, guessedLetters, incorrectLettersLength }: HangmanWordProp) {
 
     return (
         <div className="hangman-word-container">
             {wordToGuess.split('').map((letter, idx) => {
+                const toggleActive = guessedLetters.includes(letter) ? 'active' : 'inactive'
+                const toggleReveal = !guessedLetters.includes(letter) && incorrectLettersLength >= 6 ? 'reveal-on-fail' : ''
+
                 return (
                     <span key={idx} className="letter-styles">
                         <img
-                            className={guessedLetters.includes(letter) ? 'active' : 'inactive'}
+                            className={`
+                                ${toggleActive}
+                                ${toggleReveal}
+                            `}
                             src={`/omori-hangman-keys/${letter}.webp`}
                             alt={letter} />
                     </span>
