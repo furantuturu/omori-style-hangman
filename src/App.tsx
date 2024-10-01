0d431066bac4a44df5d34d7b5a4648d617c4e48b
Haemoffy <fuwansuu@gmail.com>
2024-10-01 22:29:22 +0800

    changes: - made the main 2 component ;azy loaded - added win or lose indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,35 @@
-import { useCallback, useEffect, useState } from "react";
-import HangmanModel from "./HangmanModel";
-import HangmanWord from "./HangmanWord";
+import React, { lazy, useState } from "react";
+import useGuessedLetter from "./assets/custom-hooks/useGuessedLetter";
 import words from './wordlist.json'
 import "./assets/css/App.css";
 
+const HangmanModel = lazy(() => import('./HangmanModel'))
+const HangmanWord = lazy(() => import('./HangmanWord'))
+
 function App() {
-  const [wordToGuess, setwordToGuess] = useState(() => {
+  const [wordToGuess, setwordToGuess] = useState<string>(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  const addGuessedLetter = useCallback((letter: string) => {
-    if (guessedLetters.includes(letter)) return
-
-    setGuessedLetters(prevLetter => [...prevLetter, letter])
-  }, [guessedLetters])
-
-  useEffect(() => {
-    const handleKeyTrigger = (e: KeyboardEvent) => {
-      const key = e.key
-      if (!key.match(/^[a-z]$/)) return
-
-      addGuessedLetter(key)
-    }
-
-    document.addEventListener('keypress', handleKeyTrigger)
-
-    return () => {
-      document.removeEventListener('keypress', handleKeyTrigger)
-    }
-  }, [guessedLetters])
+  const { guessedLetters, incorrectLetters, isWin, isLose } = useGuessedLetter(wordToGuess);
 
   return (
     <div className="container">
-      <HangmanModel />
-      <HangmanWord wordToGuess={wordToGuess} guessedLetters={guessedLetters} />
+      <h1 style={{ position: 'absolute', top: '10px', left: '10px' }}>Type any alphabetical key</h1>
+      {isWin &&
+        (<dialog open>Winner</dialog>)
+      }
+      {isLose &&
+        (<dialog open>Lose</dialog>)
+      }
+      <React.Suspense fallback={<h1>Loading...</h1>}>
+        <HangmanModel incorrectLettersLength={incorrectLetters.length} />
+        <HangmanWord
+          wordToGuess={wordToGuess}
+          guessedLetters={guessedLetters}
+          incorrectLettersLength={incorrectLetters.length}
+        />
+      </React.Suspense>
     </div>
   )
 }
