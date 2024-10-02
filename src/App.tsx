5f5e415fd2c6df5ae1219216fab1fc75f23a18b3
Haemoffy <fuwansuu@gmail.com>
2024-10-02 22:24:04 +0800

    changes: - wordToGuess state logic transfered into helper to be reusable - implemented the Modal component to flash win or lose msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import React, { lazy, useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import useGuessedLetter from "./assets/custom-hooks/useGuessedLetter";
-import words from './wordlist.json'
 import "./assets/css/App.css";
+import getWord from "./assets/helpers/getWord";
 
+const Modal = lazy(() => import('./Modal'))
 const HangmanModel = lazy(() => import('./HangmanModel'))
 const HangmanWord = lazy(() => import('./HangmanWord'))
 
 function App() {
-  const [wordToGuess, setwordToGuess] = useState<string>(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
+  const [wordToGuess, setWordToGuess] = useState<string>(getWord())
 
-  const { guessedLetters, incorrectLetters, isWin, isLose } = useGuessedLetter(wordToGuess);
+  const { guessedLetters, setGuessedLetters, incorrectLetters, isWin, isLose } = useGuessedLetter(wordToGuess);
 
   return (
     <div className="container">
       <h1 style={{ position: 'absolute', top: '10px', left: '10px' }}>Type any alphabetical key</h1>
-      {isWin &&
-        (<dialog open>Winner</dialog>)
-      }
-      {isLose &&
-        (<dialog open>Lose</dialog>)
-      }
-      <React.Suspense fallback={<h1>Loading...</h1>}>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        {isWin || isLose ? (
+          <Modal
+            isWin={isWin}
+            isLose={isLose}
+            setGuessedLetters={setGuessedLetters}
+            getWord={getWord}
+            setWordToGuess={setWordToGuess}
+          />
+        ) : ''
+        }
         <HangmanModel incorrectLettersLength={incorrectLetters.length} />
         <HangmanWord
           wordToGuess={wordToGuess}
           guessedLetters={guessedLetters}
           incorrectLettersLength={incorrectLetters.length}
         />
-      </React.Suspense>
+      </Suspense>
     </div>
   )
 }
