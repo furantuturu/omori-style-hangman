0fbdbc98c1dbf13c0b8dcfbca502fe99dae06dcf
Haemoffy <fuwansuu@gmail.com>
2024-10-04 23:32:51 +0800

    added the sfx for hangman model

--- src/HangmanModel.tsx
+++ src/HangmanModel.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useRef } from "react"
+
 type HangmanModelProp = {
     incorrectLettersLength: number
 }
 
 function HangmanModel({ incorrectLettersLength }: HangmanModelProp) {
+    const sfx = useRef<HTMLAudioElement>(null)
+
+    useEffect(() => {
+        if (incorrectLettersLength < 1) return
+
+        sfx.current?.play()
+    }, [incorrectLettersLength])
+
     return (
         <div style={{ border: '5px solid white' }}>
             {incorrectLettersLength <= 6 &&
                 (<img src={`/omori-hangman-imgs/${incorrectLettersLength}-attempt.svg`} />)
             }
+            <audio src="/audios/hangmanModelSFX.wav" ref={sfx}></audio>
         </div>
     )
 }
