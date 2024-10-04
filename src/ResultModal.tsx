576f9ea7ced4f83cea62706d2a5244311658d3d3
Haemoffy <fuwansuu@gmail.com>
2024-10-04 23:27:36 +0800

    component modal for displaying win or lose

---
+++ src/ResultModal.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react"
+import './assets/css/modals.css'
+
+type ModalProp = {
+    isWin: boolean,
+    isLose: boolean,
+    getWord: () => string,
+    setGuessedLetters: React.Dispatch<React.SetStateAction<string[]>>,
+    setWordToGuess: React.Dispatch<React.SetStateAction<string>>,
+}
+
+function ResultModal({ isWin, isLose, setGuessedLetters, getWord, setWordToGuess }: ModalProp) {
+    const modalRef = useRef<HTMLDialogElement>(null)
+
+    useEffect(() => {
+        modalRef.current?.showModal()
+    }, [])
+
+    function handleRestartModal() {
+        setGuessedLetters([])
+        setWordToGuess(getWord())
+        modalRef.current?.close()
+    }
+
+    return (
+        <dialog className="modal result-modal" ref={modalRef}>
+            <h1>
+                {isWin && "You guessed the word!"}
+                {isLose && "You failed to guess the word..."}
+            </h1>
+            <audio src={`/audios/${isWin ? 'win' : 'lose'}SFX.wav`} autoPlay />
+            <button onClick={handleRestartModal}>Restart</button>
+        </dialog>
+    )
+}
+
+export default ResultModal
