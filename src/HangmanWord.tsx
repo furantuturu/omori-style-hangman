d5fdaf0ea2302fe843b7425705e90a1db63d672d
Haemoffy <fuwansuu@gmail.com>
2024-10-08 21:22:34 +0800

    reverted back to using index as key

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -10,12 +10,12 @@ function HangmanWord({ wordToGuess, guessedLetters, incorrectLettersLength }: Re
 
     return (
         <div className="hangman-word-container">
-            {wordToGuess.split('').map(letter => {
+            {wordToGuess.split('').map((letter, idx) => {
                 const toggleActive = guessedLetters.includes(letter) ? 'active' : 'inactive'
                 const toggleReveal = !guessedLetters.includes(letter) && incorrectLettersLength >= 6 ? 'reveal-on-fail' : ''
 
                 return (
-                    <span key={letter} className="letter-styles">
+                    <span key={idx} className="letter-styles">
                         <img
                             className={`${toggleActive} ${toggleReveal}`}
                             src={`/omori-hangman-keys/${letter}.webp`}
