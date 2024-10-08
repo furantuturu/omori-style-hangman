5cffd6d434751828ef36a6534f6db2a764ae59f8
Haemoffy <fuwansuu@gmail.com>
2024-10-08 20:56:55 +0800

    added AbortController for clean-up

--- src/assets/custom-hooks/useGetWordMeanings.ts
+++ src/assets/custom-hooks/useGetWordMeanings.ts
@@ -13,11 +13,20 @@ export default function useGetWordMeanings(wordToGuess: string) {
     const [wordMeanings, setWordMeanings] = useState<WordMeaning[]>([])
 
     useEffect(() => {
-        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToGuess}`)
+        const controller = new AbortController()
+        const signal = controller.signal
+
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToGuess}`, { signal })
             .then(res => res.json())
             .then(data => {
                 setWordMeanings(data[0].meanings)
+            }).catch(err => {
+                if (err.name === "AbortError") return console.log("Cancelled Request")
             })
+
+        return () => {
+            controller.abort()
+        }
     }, [wordToGuess])
 
     return wordMeanings
